17d86f3965980aa246d7d672e34aaaa5d1f84d4d
Lucas Ramos <lucasrcmmadruga@gmail.com>
2018-11-27 19:54:30 -0200

    tela login, method login de adm

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \NC\Page;
 use \NC\PageAdm;
+use \NC\Models\User;
 
 $app = new Slim();
 
@@ -20,9 +23,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+    User::verifyLogin();
+
 	$page =new PageAdm();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login',function(){
+
+	$page = new PageAdm(["header"=>false,"footer"=>false
+	
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->get('/admin/logout',function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
 
 });
 
